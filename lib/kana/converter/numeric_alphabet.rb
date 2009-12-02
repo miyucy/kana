049ab9330ec6821fa25413bff4de993e4880a0c7
fistfvck <fistfvck@gmail.com>
2009-12-02 13:20:54 +0900

    互換性向上

--- lib/kana/converter/numeric_alphabet.rb
+++ lib/kana/converter/numeric_alphabet.rb
@@ -66,12 +66,10 @@ Kana::Converter::ALPHABET = Hash[*{0xFF21 => 0x41, # Fullwidth Latin Capital Let
                                  }.map{ |k,v| [[k].pack('U'), [v].pack('U')] }.flatten]
 
 Kana::Converter::SYMBOL = Hash[*{0xFF01 => 0x21, # Fullwidth Exclamation Mark
-                                 # 0xFF02 => 0x22, # Fullwidth Quotation Mark
                                  0xFF03 => 0x23, # Fullwidth Number Sign
                                  0xFF04 => 0x24, # Fullwidth Dollar Sign
                                  0xFF05 => 0x25, # Fullwidth Percent Sign
                                  0xFF06 => 0x26, # Fullwidth Ampersand
-                                 # 0xFF07 => 0x27, # Fullwidth Apostrophe
                                  0xFF08 => 0x28, # Fullwidth Left Parenthesis
                                  0xFF09 => 0x29, # Fullwidth Right Parenthesis
                                  0xFF0A => 0x2A, # Fullwidth Asterisk
@@ -81,6 +79,16 @@ Kana::Converter::SYMBOL = Hash[*{0xFF01 => 0x21, # Fullwidth Exclamation Mark
                                  0x2212 => 0x2D,
                                  0xFF0E => 0x2E, # Fullwidth Full Stop
                                  0xFF0F => 0x2F, # Fullwidth Solidus
+                                 0xFF10 => 0x30,
+                                 0xFF11 => 0x31,
+                                 0xFF12 => 0x32,
+                                 0xFF13 => 0x33,
+                                 0xFF14 => 0x34,
+                                 0xFF15 => 0x35,
+                                 0xFF16 => 0x36,
+                                 0xFF17 => 0x37,
+                                 0xFF18 => 0x38,
+                                 0xFF19 => 0x39,
                                  0xFF1A => 0x3A, # Fullwidth Colon
                                  0xFF1B => 0x3B, # Fullwidth Semicolon
                                  0xFF1C => 0x3C, # Fullwidth Less-Than Sign
@@ -88,27 +96,70 @@ Kana::Converter::SYMBOL = Hash[*{0xFF01 => 0x21, # Fullwidth Exclamation Mark
                                  0xFF1E => 0x3E, # Fullwidth Greater-Than Sign
                                  0xFF1F => 0x3F, # Fullwidth Question Mark
                                  0xFF20 => 0x40, # Fullwidth Commercial At
+                                 0xFF21 => 0x41,
+                                 0xFF22 => 0x42,
+                                 0xFF23 => 0x43,
+                                 0xFF24 => 0x44,
+                                 0xFF25 => 0x45,
+                                 0xFF26 => 0x46,
+                                 0xFF27 => 0x47,
+                                 0xFF28 => 0x48,
+                                 0xFF29 => 0x49,
+                                 0xFF2A => 0x4A,
+                                 0xFF2B => 0x4B,
+                                 0xFF2C => 0x4C,
+                                 0xFF2D => 0x4D,
+                                 0xFF2E => 0x4E,
+                                 0xFF2F => 0x4F,
+                                 0xFF30 => 0x50,
+                                 0xFF31 => 0x51,
+                                 0xFF32 => 0x52,
+                                 0xFF33 => 0x53,
+                                 0xFF34 => 0x54,
+                                 0xFF35 => 0x55,
+                                 0xFF36 => 0x56,
+                                 0xFF37 => 0x57,
+                                 0xFF38 => 0x58,
+                                 0xFF39 => 0x59,
+                                 0xFF3A => 0x5A,
                                  0xFF3B => 0x5B,
                                  0xFF3D => 0x5D,
                                  0xFF3E => 0x5E,
                                  0xFF3F => 0x5F,
                                  0xFF40 => 0x60,
+                                 0xFF41 => 0x61,
+                                 0xFF42 => 0x62,
+                                 0xFF43 => 0x63,
+                                 0xFF44 => 0x64,
+                                 0xFF45 => 0x65,
+                                 0xFF46 => 0x66,
+                                 0xFF47 => 0x67,
+                                 0xFF48 => 0x68,
+                                 0xFF49 => 0x69,
+                                 0xFF4A => 0x6A,
+                                 0xFF4B => 0x6B,
+                                 0xFF4C => 0x6C,
+                                 0xFF4D => 0x6D,
+                                 0xFF4E => 0x6E,
+                                 0xFF4F => 0x6F,
+                                 0xFF50 => 0x70,
+                                 0xFF51 => 0x71,
+                                 0xFF52 => 0x72,
+                                 0xFF53 => 0x73,
+                                 0xFF54 => 0x74,
+                                 0xFF55 => 0x75,
+                                 0xFF56 => 0x76,
+                                 0xFF57 => 0x77,
+                                 0xFF58 => 0x78,
+                                 0xFF59 => 0x79,
+                                 0xFF5A => 0x7A,
                                  0xFF5B => 0x7B, # Fullwidth Left Curly Bracket
                                  0xFF5C => 0x7C, # Fullwidth Vertical Line
                                  0xFF5D => 0x7D, # Fullwidth Right Curly Bracket
-                                 # 0xFF5E => 0x7E, # Fullwidth Tilde
-                                 # 0xFF5F => 0x2985, # Fullwidth Left White Parenthesis
-                                 # 0xFF60 => 0x2986, # Fullwidth Right White Parenthesis
-                                 # 0xFFE0 => 0xA2, # Fullwidth Cent Sign
-                                 # 0xFFE1 => 0xA3, # Fullwidth Pound Sign
-                                 # 0xFFE2 => 0xAC, # Fullwidth Not Sign
-                                 # 0xFFE3 => 0xAF, # Fullwidth Macron
-                                 # 0xFFE4 => 0xA6, # Fullwidth Broken Bar
-                                 # 0xFFE5 => 0xA5, # Fullwidth Yen Sign
-                                 # 0xFFE6 => 0x20A9, # Fullwidth Won Sign
                                }.map{ |k,v| [[k].pack('U'), [v].pack('U')] }.flatten]
 
-Kana::Converter::SYMBOL_INVERT = Hash[*{0x2D => 0xFF0D}.map{ |k,v| [[k].pack('U'), [v].pack('U')] }.flatten]
+Kana::Converter::SYMBOL_INVERT = Hash[*{0x2D => 0xFF0D
+                                      }.map{ |k,v| [[k].pack('U'), [v].pack('U')] }.flatten]
 
 Kana::Converter::SPACE = Hash[*{0x3000 => 0x20, # IDEOGRAPHIC SPACE
                               }.map{ |k,v| [[k].pack('U'), [v].pack('U')] }.flatten]
